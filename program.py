7ff9075313450db302ec59851056d42c3c1154d8
Anton Fedorov <75174420+freelomen@users.noreply.github.com>
2022-09-29 21:41:48 +0500

    Update program.py
    Someone, may be I, added a new line

--- program.py
+++ program.py
@@ -1,7 +1,8 @@
 def main():
     print('My first git program')
     print('And I change it every day')
-    print('We need more prints')
+    print('We need more prints')    
+    print('UFO came and added this line')
 
 
 if __name__ == '__main__':
